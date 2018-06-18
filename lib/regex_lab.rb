189e91510a626bb1e5af1ce9d5c5a90859e2d4fe
jgurowsky@gmail.com <jgurowsky@gmail.com>
2018-06-18 21:50:35 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-
+  word.scan(/^[aeiouAEIOU]\w*?$/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w*?ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\w{5}/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match(/^[A-Z]\w*?\W$/)
 end
 
 def valid_phone_number?(phone)
-
+  phone.scan(/^\(?(\d{3})\)?([a-zA-Z]{7})?[\s-]?\d+[\s-]?\d+$/)
 end
